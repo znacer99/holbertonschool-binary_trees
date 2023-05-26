91c02b63bc01a83cf4292969e55fcb423f729152
root <6274@holbertonstudents.com>
2023-05-26 07:09:42 -0700

    size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_size - measures the deptg of a node in a binary tree.
+* @tree: pointer to the node to measure the depth.
+*
+* Return: If tree is NULL, return 0.
+*/
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+
+
+if (tree == NULL)
+return (0);
+
+return ((binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right)));
+}
